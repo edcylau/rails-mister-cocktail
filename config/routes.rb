a141cd7481b0d49d5b087c25296365d3906f33c2
Edmund Lau <tmlcyaa@gmail.com>
2019-11-22 15:57:31 +0800

    cocktail cards created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'doses/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
   resources :cocktails, only: [:new, :create, :show, :destroy] do
